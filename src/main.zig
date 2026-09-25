97a1bb0361495cc3c20dda61921bb4e350c3d49f
xap4o <kleshney@gmail.com>
2026-09-25 22:50:19 +0200

    update to zif 0.16.0

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,9 @@ const std = @import("std");
 const zuuid = @import("zuuid");
 const uuid = @import("uuid");
 
-pub fn main() !void {
-    const id = uuid.v4.new();
+pub fn main(init: std.process.Init) !void {
+    const id = uuid.v4.new(init.io);
     const str = uuid.urn.serialize(id);
-    try zuuid.println(&str);
+    try zuuid.println(init.io, &str);
 }
 
--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
-pub fn println(str: []const u8) !void {
+pub fn println(io: std.Io, str: []const u8) !void {
     var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
     const stdout = &stdout_writer.interface;
     try stdout.print("{s}\n", .{str});
     try stdout.flush();
